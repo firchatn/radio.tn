50e60b9df82506c2c8aff7752964bd5e17333140
Moez Bouhlel <bmoez.j@gmail.com>
2017-10-09 15:29:05 +0100

    update player

--- player.js
+++ player.js
@@ -1,65 +1,79 @@
-class Particle {
-    constructor(index, parent) {
-        this.index = index;
-        this.parent = parent;
-        this.minSize = 5;
-        this.init();
-    }
-    init() {
-        this.freqVal = this.parent.freqData[this.index] * 0.01;
-        this.size =
-            this.freqVal *
-            ((this.parent.dimensions.x + this.parent.dimensions.y) * 0.5) *
-            0.0125 +
-            this.minSize;
-        this.position = new Vector2(
-            Math.random() * this.parent.dimensions.x,
-            this.parent.dimensions.y + this.size
-        );
-        this.velocity = new Vector2(2 - Math.random() * 4, 0);
-    }
-    update() {
-        this.freqVal = this.parent.freqData[this.index] * 0.01;
-
-        this.size = this.freqVal * 20 + this.minSize;
-
-        this.hue =
-            this.index / this.parent.bufferLen * 360 + 120 + this.parent.tick / 6;
-        this.saturation = this.freqVal * 50;
-        this.alpha = this.freqVal * 0.3;
-
-        this.fill = `hsla(${this.hue}, ${this.saturation}%, 50%, ${this.alpha})`;
-        this.lift = Math.pow(this.freqVal, 3);
-
-        this.position.subY(this.lift + 0.5);
-        this.position.add(this.velocity);
-
-        this.checkBounds();
-    }
-    checkBounds() {
-        if (
-            this.position.y < -this.size ||
-            this.position.x < -this.parent.dimensions.x * 0.15 ||
-            this.position.x > this.parent.dimensions.x * 1.15
-        ) {
-            this.init();
-        }
-    }
-}
+const radios = [{
+    title: "I FM",
+    src: "http://5.135.138.182:8000/direct",
+    img: "./imgs/ifm.png",
+    url: "https://www.ifm.tn/",
+}, {
+    title: "Oxygene FM",
+    src: "http://radiooxygenefm.ice.infomaniak.ch/radiooxygenefm-64.mp3",
+    img: "./imgs/radiooxygene.png",
+    url: "http://www.radiooxygene.tn/",
+}, {
+    title: "Express FM",
+    src: "http://expressfm.ice.infomaniak.ch/expressfm-64.mp3",
+    img: "./imgs/radioexpressfm.png",
+    url: "http://www.radioexpressfm.com/",
+}, {
+    title: "Sabra FM",
+    src: "http://stream6.tanitweb.com/sabrafm",
+    img: "./imgs/radiosabrafm.png",
+    url: "http://www.radiosabrafm.net/",
+}, {
+    title: "Shems FM",
+    src: "http://stream6.tanitweb.com/shems",
+    img: "./imgs/shemsfm.png",
+    url: "http://shemsfm.net:8080/",
+}, {
+    title: "Mosaique FM",
+    src: "http://radio.mosaiquefm.net:8000/mosalive",
+}, {
+    title: "Jawhara FM",
+    src: "http://streaming2.toutech.net:8000/jawharafm",
+}, {
+    title: "Zitouna FM",
+    src: "http://stream8.tanitweb.com/zitounafm",
+}, {
+    title: "Knooz FM",
+    src: "http://streaming.knoozfm.net:8000/knoozfm",
+}, {
+    title: "Cap FM",
+    src: "http://stream8.tanitweb.com/capfm",
+}, {
+    title: "Diwan FM",
+    src: "http://stream8.tanitweb.com/diwanfm",
+}, {
+    title: "Misk FM",
+    src: "http://178.32.253.134:8000/stream?type=.mp3",
+}, {
+    title: "Radio Med",
+    src: "http://stream6.tanitweb.com/radiomed",
+}, {
+    title: "Oasis FM",
+    src: "http://stream6.tanitweb.com/oasis",
+}, {
+    title: "Ulysse FM",
+    src: "http://188.165.79.152:9990/;stream.mp3",
+}, {
+    title: "Saraha FM",
+    src: "http://ns326208.ip-37-59-9.eu:8000/sarahafm",
+}, {
+    title: "MFM FM",
+    src: "http://92.222.225.69:9300/;",
+}, {
+    title: "Nejma FM",
+    src: "http://188.166.109.186:8000/stream",
+}, {
+    title: "Radio 6",
+    src: "http://streaming.radio6tunis.net:8000/radio6tunis",
+}];
 
 class App {
     constructor() {
         this.globalMovement = new Vector2();
-        this.initCanvas();
         this.initAudio();
         this.initUI();
+        this.initPlaylist();
         this.loadAudio();
-        this.populate();
-    }
-    initCanvas() {
-        this.tick = 0;
-        this.dark = false;
-        this.dimensions = {};
     }
     initUI() {
         this.controls = {
@@ -69,15 +83,15 @@ class App {
             volume: document.querySelector("#btn-volume")
         };
         this.controls.prev.onclick = () => {
-            this.currentSong = this.currentSong > 1 ?
+            this.currentSong = this.currentSong > 0 ?
                 this.currentSong - 1 :
-                this.fileNames.length;
+                radios.length - 1;
             this.loadAudio();
         };
         this.controls.next.onclick = () => {
-            this.currentSong = this.currentSong < this.fileNames.length ?
+            this.currentSong = this.currentSong < radios.length - 1 ?
                 this.currentSong + 1 :
-                1;
+                0;
             this.loadAudio();
         };
         this.controls.play.onclick = () => {
@@ -117,48 +131,34 @@ class App {
         };
         this.background = document.getElementById("background");
         this.title = document.getElementById("title");
+        this.playlist = document.getElementById("playlist");
+      this.playlist.onclick = (e) => {
+        if (!e.target.dataset.idx) {
+          return;
+        }
+        this.currentSong = parseInt(e.target.dataset.idx);
+        this.loadAudio();
+      };
+    }
+    initPlaylist() {
+        let idx = 0;
+        for (const radio of radios) {
+          const item = document.createElement("li");
+          const img = document.createElement("img");
+          img.src = radio.img;
+          img.dataset.idx = idx;
+          item.dataset.idx = idx;
+          item.appendChild(img);
+          this.playlist.appendChild(item);
+          idx++;
+        }
     }
     initAudio() {
-        this.currentSong = 1;
+        this.currentSong = 0;
         this.volume = 1;
-        this.baseURL = "https://box1182.BlueHost.com/~seanale3/codepen/audio/";
-        this.fileNames = [
-            "dmwaltz.mp3",
-            "nocturne92.mp3",
-            "mozart25.mp3",
-            "trista.mp3",
-            "waltzflowers.mp3"
-        ];
-        this.songTitles = [
-            "Dmitri Shostakovich - Waltz No. 2",
-            "Frederic Chopin - Nocturne op. 9 no. 2",
-            "Mozart - Symphony no. 25",
-            "Heitor Villa-Lobos - Tristorosa",
-            "Pyotr Tchaikovsky - Waltz of the Flowers"
-        ];
 
         this.audio = document.getElementById("audio");
-        this.audio.addEventListener("ended", () => {
-            this.audio.currentTime = 0;
-            this.audio.pause();
-            this.currentSong = this.currentSong < this.fileNames.length ?
-                this.currentSong + 1 :
-                1;
-            this.loadAudio();
-        });
-        this.audioCtx = new AudioContext();
 
-        this.source = this.audioCtx.createMediaElementSource(this.audio);
-        this.gainNode = this.audioCtx.createGain();
-        this.analyser = this.audioCtx.createAnalyser();
-
-        this.source.connect(this.gainNode);
-        this.gainNode.connect(this.analyser);
-        this.analyser.connect(this.audioCtx.destination);
-
-        this.gainNode.gain.value = this.volume;
-        this.bufferLen = 512;
-        this.freqData = new Uint8Array(this.bufferLen);
     }
     loadAudio() {
         let request = new XMLHttpRequest();
@@ -171,25 +171,14 @@ class App {
         this.controls.next.classList.add("disabled");
         this.controls.play.classList.add("disabled");
 
-        request.open(
-            "GET",
-            this.baseURL + this.fileNames[this.currentSong - 1],
-            true
-        );
-        request.responseType = "blob";
-
-        request.onload = () => {
-            this.playAudio(request.response);
-        };
-
-        request.send();
+        this.playAudio(radios[this.currentSong].src);
     }
-    playAudio(data) {
+    playAudio(url) {
         this.audioReady = true;
         this.playing = true;
 
         this.background.classList.remove("loading");
-        this.title.innerHTML = this.songTitles[this.currentSong - 1];
+        this.title.innerHTML = radios[this.currentSong].title;
 
         this.controls.prev.classList.remove("disabled");
         this.controls.next.classList.remove("disabled");
@@ -198,29 +187,11 @@ class App {
         this.controls.play.getElementsByTagName("i")[0].classList.remove("fa-play");
         this.controls.play.getElementsByTagName("i")[0].classList.add("fa-pause");
 
-        this.audio.src = window.URL.createObjectURL(data);
+        this.audio.src = url;
+        this.audio.load();
         this.audio.play();
     }
-    populate() {
-        this.particles = [];
-        for (let i = 0; i < this.bufferLen; i++) {
-            this.particles.push(new Particle(i, this));
-        }
-    }
 }
-
-window.requestAnimationFrame = (() => {
-    return (
-        window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function(callback) {
-            window.setTimeout(callback, 1000 / 60);
-        }
-    );
-})();
 
 window.onload = () => {
     let app = new App();
